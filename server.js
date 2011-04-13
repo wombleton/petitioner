9d7175d76433c0fa3aba5f90dae3cb9d96f1aa47
Rowan Crawford <wombleton@gmail.com>
2011-04-14 02:40:52 +1200

    Refine logging in process.

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ app.configure(function() {
   app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: config.session_secret }));
   app.use(express.static(__dirname + '/static'));
   app.use(easyoauth(require('./keys_file')));
+  app.use(app.router);
 });
 
 app.configure('development', function() {
@@ -17,17 +20,38 @@ app.configure('development', function() {
     dumpExceptions: true,
     showStack: true
   }));
-  port = 3000;
 });
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
+  var details = req.getAuthDetails();
+  console.log(details);
   res.render('index.jade', {
     locals: {
-      title: config.title
+      title: config.title,
+      twitter: details && details.user && details.user.username,
+      facebook: details && details.user && details.user.name
     }
+  });
+});
+
+app.get ('/logout', function(req, res, params) {
+  req.logout();
+  res.writeHead(303, { 'Location': "/" });
+  res.end('');
+})
+
+app.get('/authenticated/', function(req, res) {
+  req.authenticate(['facebook'], function(err, authenticated) {
+    res.redirect('/');
+  });
+});
+
+app.get('/authenticated', function(req, res) {
+  req.authenticate(['twitter'], function(err, authenticated) {
+    res.redirect('/');
   });
 });
 
