b3be413bd187ef6e67690a07eb1950dc6b512963
Rowan Crawford <wombleton@gmail.com>
2011-04-15 23:22:44 +1200

    Set explanations in config file.

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports.cfg = {
-  facebook_id: '',
-  facebook_secret: '',
-  facebook_callback: '',
-  twitter_key: '',
-  twitter_secret: '',
-  twitter_callback: '',
-  title: 'Enlist'
+  facebook_id: '$YOUR_APPLICATION_FB_ID',
+  facebook_secret: '$YOUR_APPLICATION_FB_SECRET',
+  facebook_callback: '$YOUR_SITE_HERE/authed/facebook/',
+  twitter_key: '$YOUR_APPLICATION_TWITTER_KEY',
+  twitter_secret: '$YOUR_APPLICATION_TWITTER_SECRET',
+  twitter_callback: '$YOUR_SITE_HERE/authed/twitter/',
+  title: '$YOUR_PETITION_TITLE',
+  session_secret: '$SESSION_SECRET'
 }
