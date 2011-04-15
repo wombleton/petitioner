f128c6130e08c5d786aa7a6dc7334dcbbb7fe16b
Rowan Crawford <wombleton@gmail.com>
2011-04-15 22:45:25 +1200

    Fix up auth and signup savings

--- server.js
+++ server.js
@@ -30,27 +30,46 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 function getAuth(req) {
-  var details = req.getAuthDetails();
-  return {
-    name: 'abcd',
-    url: 'http://fake.url/abcd'
+  var details = req.getAuthDetails(),
+      facebookish,
+      twitterish;
+  if (details.user) {
+    twitterish = details.user.user_id && details.user.username;
+    facebookish = details.user.name && details.user.link;
+    if (twitterish) {
+      return {
+        name: details.user.username,
+        url: 'https://twitter.com/' + details.user.username
+      }
+    } else if (facebookish) {
+      return {
+        name: details.user.name,
+        url: details.user.link
+      }
+    }
+  } else {
+    return undefined;
   }
+
 }
 
 app.get('/', function(req, res) {
   var details = getAuth(req);
   fs.readFile('petition.md', function(err, file) {
-    res.render('index.jade', {
-      locals: {
-        title: config.title,
-        user: details,
-        petition: md(file.toString())
-      }
+    signups.get('doc', function(err, doc) {
+      res.render('index.jade', {
+        locals: {
+          title: config.title,
+          user: details,
+          petition: md(file.toString()),
+          signups: doc && doc.signups || []
+        }
+      });
     });
   });
 });
 
-app.get ('/retract', function(req, res, params) {
+app.get ('/undo', function(req, res, params) {
   var details = getAuth(req);
   removeSignup(details.url, function() {
     req.logout();
@@ -60,42 +79,79 @@ app.get ('/retract', function(req, res, params) {
 });
 
 function removeSignup(url, callback) {
-  var list = signups.get('list') || {};
-  delete list[url];
-  signups.put('list', list, callback);
-}
+  signups.get('doc', function(err, doc) {
+    var i,
+        signup;
+    if (err) {
+      callback();
+    } else {
+      for (i = 0; i < doc.signups.length; i++) {
+        signup = doc.signups[i];
+        if (signup.url === url) {
+          doc.signups.splice(i, 1);
+          break;
+        }
+      }
+      signups.put('doc', doc, callback);
+    }
+  });
 
+}
 
 function saveSignup(name, url, callback) {
-  var list = signups.get('list') || {};
-  list[url] = {
-    name: name,
-    url: url,
-    date: new Date()
-  };
-  signups.put('list', list, callback)
+  var add = true,
+      i,
+      doc = signups.getSync('doc') || { signups: [] },
+      ok,
+      signup;
+  for (i = 0; i < doc.signups.length; i++) {
+    signup = doc.signups[i];
+    if (signup.url === url) {
+      add = false;
+      break;
+    }
+  }
+
+  if (add) {
+    doc.signups.push({
+      name: name,
+      url: url,
+      date: new Date()
+    });
+  }
+  ok = signups.putSync('doc', doc);
+  if (ok) {
+    callback();
+  } else {
+    res.setHeader(500, {});
+    res.end('<html><h1>Something went wrong. Please try again.</h1></html>');
+  }
 }
 
-app.get('/authenticated', function(req, res) {
-  var details,
-      mode = req.params.mode,
-      name,
-      url;
-  req.authenticate([mode], function(err, authenticated) {
+app.get('/authed/twitter/', function(req, res) {
+  var details;
+  req.authenticate(['twitter'], function(err, authenticated) {
     if (authenticated) {
       details = getAuth(req);
-      if (mode === 'twitter') {
-        name = details.name;
-        url = details.url;
-      } else if (mode === 'facebook') {
-        name = details.name;
-        url = details.url;
-      }
-      saveSignup(name, url, function() {
+      saveSignup(details.name, details.url, function() {
         res.redirect('/');
       })
     } else {
-      res.redirect('/loginfail');
+      res.end(' <html><h1>Twitter authentication failed :( </h1></html>')
+    }
+  });
+});
+
+app.get('/authed/facebook/', function(req, res) {
+  var details;
+  req.authenticate(['facebook'], function(err, authenticated) {
+    if (authenticated) {
+      details = getAuth(req);
+      saveSignup(details.name, details.url, function() {
+        res.redirect('/');
+      })
+    } else {
+      res.end('<html><h1>Facebook authentication failed :( </h1></html>')
     }
   });
 });
